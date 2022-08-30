58cf2f2089f1bebe6b14888bac226b5316fb6066
Odhran <mahonyodhran@gmail.com>
2022-08-30 21:20:50 +0100

    miff st jeor returns bmr which is used to calc tdee in new method which outputs TDEEs

--- methods/equations.py
+++ methods/equations.py
@@ -4,3 +4,14 @@ def mifflin_st_jeor(age, height, weight, gender):
     else:
         bmr = (10*weight)+(6.25*height)-(5*age) - 161
     print('\nYour BMR is: {} calories'.format(int(bmr)))
+    return bmr
+
+def tdee(bmr):
+    values = {'Sedentary': 1.2,
+              'Lightly Active' : 1.375,
+              'Moderately Active' : 1.55,
+              'Very Active' : 1.725,
+              'Extremely Active' : 1.9}
+    
+    for x,y in values.items():
+        print('Your TDEE for {} lifestyle is: {} calories'.format(x,int(bmr * y)))
