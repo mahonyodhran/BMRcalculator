8aea2f4ef217c07799780a23968103cc2221910a
Odhran <mahonyodhran@gmail.com>
2022-08-30 20:25:30 +0100

    updated driver class to use separate files

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('Hello World')
+from equations import mifflin_st_jeor
+from inputs import getFloat, getChar
+
+age = getFloat('age')
+height = getFloat('height')
+weight = getFloat('weight')
+gender = getChar()
+
+mifflin_st_jeor(age, height, weight, gender)
