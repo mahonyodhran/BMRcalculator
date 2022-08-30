8c9e1d0054e55512e4c527e1a445b4b5cdce119a
Odhran <mahonyodhran@gmail.com>
2022-08-30 21:22:10 +0100

    updated imports/methods to new names - added extra flow at end for TDEE

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from methods.equations import mifflin_st_jeor
-from methods.inputs import getFloat, getChar
+from methods.equations import mifflin_st_jeor, tdee
+from methods.inputs import get_float, get_char, tdee_info
 
-age = getFloat('age')
-height = getFloat('height')
-weight = getFloat('weight')
-gender = getChar()
+age = get_float('age')
+height = get_float('height')
+weight = get_float('weight')
+gender = get_char()
 
-mifflin_st_jeor(age, height, weight, gender)
+bmr = mifflin_st_jeor(age, height, weight, gender)
+tdee(bmr)
+info = input('Would you like more info on TDEE? [y/N]')
+if info.lower() =='y':
+    tdee_info() 
+print('Thanks for using, goodbye!')
