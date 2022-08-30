4e3382b9c5da7bf9427c42693629ac1bf4164d00
Odhran <mahonyodhran@gmail.com>
2022-08-30 20:38:52 +0100

    moved dir

--- equations.py
+++
@@ -1,6 +0,0 @@
-def mifflin_st_jeor(age, height, weight, gender):
-    if(gender == 'M'):
-        bmr = (10*weight)+(6.25*height)-(5*age) + 5
-    else:
-        bmr = (10*weight)+(6.25*height)-(5*age) - 161
-    print('\nYour BMR is: {} calories'.format(int(bmr)))
--- inputs.py
+++
@@ -1,5 +0,0 @@
-def getFloat(value):
-    return float(input('Please enter your {}: '.format(value)))
-
-def getChar():
-    return input('Please enter your gender: ')
