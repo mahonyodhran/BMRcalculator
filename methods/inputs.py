b4647772ca2c5c1827a6df9422d8c51156be67f0
Odhran <mahonyodhran@gmail.com>
2022-08-30 21:21:34 +0100

    renamed methods to be more pythonic / added in tdee_info optional info method

--- methods/inputs.py
+++ methods/inputs.py
@@ -1,5 +1,12 @@
-def getFloat(value):
+def get_float(value):
     return float(input('Please enter your {}: '.format(value)))
 
-def getChar():
+def get_char():
     return input('Please enter your gender: ')
+
+def tdee_info():
+            print('1: Sedentary\
+            \n2: Lightly Active [1-3 times per week]\
+            \n3: Moderately Active [3-5 times per week]\
+            \n4: Very Active [6-7 times per week]\
+            \n5: Extremely Active [2x per day / Athlete etc.]')
