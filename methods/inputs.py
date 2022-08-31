50f5b06e62531f56e739e5f44f70c71023d8c72d
Odhran <mahonyodhran@gmail.com>
2022-08-31 20:42:48 +0100

    updated with some validation - not bulletproof

--- methods/inputs.py
+++ methods/inputs.py
@@ -1,11 +1,19 @@
 def get_float(value):
-    return float(input('Please enter your {}: '.format(value)))
+    while True:
+        try:
+            return float(input('Please enter your {}: '.format(value)))
+        except ValueError:
+            print("Your {} should be a number!".format(value))
 
-def get_char():
-    return input('Please enter your gender: ')
+def get_char(value):
+    answer = input('Please enter your {}: '.format(value))
+    if answer.lower() =='y':
+        return True
+    else:
+        return False
 
 def tdee_info():
-            print('1: Sedentary\
+            print('\n1: Sedentary\
             \n2: Lightly Active [1-3 times per week]\
             \n3: Moderately Active [3-5 times per week]\
             \n4: Very Active [6-7 times per week]\
