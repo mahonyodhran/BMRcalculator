2dd050b887284eed40ebb3d28e22e9bf84169a38
Odhran <mahonyodhran@gmail.com>
2022-09-10 16:37:18 +0100

    added User class

---
+++ User.py
@@ -0,0 +1,7 @@
+class User:
+  def __init__(self, name, age, weight, height, gender):
+    self.name = name
+    self.age = age
+    self.weight = weight
+    self.height = height
+    self.gender = gender
