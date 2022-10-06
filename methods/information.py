e3a25a0f6f5e376959c54551771985447bd3d2ac
Odhran <mahonyodhran@gmail.com>
2022-10-06 23:40:04 +0100

    added module for printing info - pylint 10/10

---
+++ methods/information.py
@@ -0,0 +1,13 @@
+'''module for any method that just prints info
+'''
+
+def tdee_info():
+    '''print out tdee information
+    '''
+    print(
+        "\n1: Sedentary\
+            \n2: Lightly Active [1-3 times per week]\
+            \n3: Moderately Active [3-5 times per week]\
+            \n4: Very Active [6-7 times per week]\
+            \n5: Extremely Active [2x per day / Athlete etc.]"
+    )
