e3da6b0d1491e685851aa2a45d4c979aab5d3a57
Odhran <mahonyodhran@gmail.com>
2022-10-06 23:52:49 +0100

    black reformat

--- methods/information.py
+++ methods/information.py
@@ -1,9 +1,9 @@
-'''module for any method that just prints info
-'''
+"""module for any method that just prints info
+"""
+
 
 def tdee_info():
-    '''print out tdee information
-    '''
+    """print out tdee information"""
     print(
         "\n1: Sedentary\
             \n2: Lightly Active [1-3 times per week]\
