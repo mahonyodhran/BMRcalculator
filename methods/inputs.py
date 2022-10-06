896982fd2b528703d5bfbfee18d79e8396f700a3
Odhran <mahonyodhran@gmail.com>
2022-10-06 23:53:18 +0100

    black reformat

--- methods/inputs.py
+++ methods/inputs.py
@@ -1,10 +1,9 @@
-'''module for any input required methods
-'''
+"""module for any input required methods
+"""
 
 
 def get_float(value):
-    '''get user input for a float amount
-    '''
+    """get user input for a float amount"""
     while True:
         try:
             return float(input(f"Please enter your {value}: "))
@@ -13,8 +12,7 @@ def get_float(value):
 
 
 def get_gender():
-    '''get user input for a gender
-    '''
+    """get user input for a gender"""
     while True:
         gender = str(input("Are you [m]ale or [f]emale? :")).lower()
         if gender == str("m") or gender == str("f"):
