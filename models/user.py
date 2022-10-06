ae0a90d55a2ef156f96c1e30ede00aef229eb0b5
Odhran <mahonyodhran@gmail.com>
2022-10-06 23:47:48 +0100

    black reformat/pylinting

--- models/user.py
+++ models/user.py
@@ -1,10 +1,12 @@
+'''module for user model
+'''
+
 class User:
-  def __init__(self, name, age, weight, height, gender):
-    self.name = name
-    self.age = age
-    self.weight = weight
-    self.height = height
-    self.gender = gender
-    
-    def __repr__(self):
-        return f"User(name={self.name!r}, age={self.age!r}, weight={self.weight!r}KG, height={self.height!r}CM, gender={self.gender!r})"
+    '''class for user
+    '''
+    def __init__(self, name, age, weight, height, gender):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+        self.gender = gender
