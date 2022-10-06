caec95049af9f2369add2476f860c9aef85cd127
Odhran <mahonyodhran@gmail.com>
2022-10-06 23:36:31 +0100

    renamed class back to User - added repr

--- User.py
+++ User.py
@@ -1,7 +1,10 @@
-class User_Profile:
+class User:
   def __init__(self, name, age, weight, height, gender):
     self.name = name
     self.age = age
     self.weight = weight
     self.height = height
     self.gender = gender
+    
+    def __repr__(self):
+        return f"User(name={self.name!r}, age={self.age!r}, weight={self.weight!r}KG, height={self.height!r}CM, gender={self.gender!r})"
