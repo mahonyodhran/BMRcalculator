015394a74192bc9ef8ec8a84513d8ef2eaba5c14
Odhran <mahonyodhran@gmail.com>
2022-10-06 23:36:06 +0100

    black reformat

--- methods/equations.py
+++ methods/equations.py
@@ -1,17 +1,20 @@
 def mifflin_st_jeor(age, height, weight, gender):
-    if(gender == 'M'):
-        bmr = (10*weight)+(6.25*height)-(5*age) + 5
+    if gender == "m":
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5
     else:
-        bmr = (10*weight)+(6.25*height)-(5*age) - 161
-    print('\nYour BMR is: {} calories'.format(int(bmr)))
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161
+    print("\nYour BMR is: {} calories".format(int(bmr)))
     return bmr
 
+
 def tdee(bmr):
-    values = {'Sedentary': 1.2,
-              'Lightly Active' : 1.375,
-              'Moderately Active' : 1.55,
-              'Very Active' : 1.725,
-              'Extremely Active' : 1.9}
-    
-    for x,y in values.items():
-        print('Your TDEE for {} lifestyle is: {} calories'.format(x,int(bmr * y)))
+    values = {
+        "Sedentary": 1.2,
+        "Lightly Active": 1.375,
+        "Moderately Active": 1.55,
+        "Very Active": 1.725,
+        "Extremely Active": 1.9,
+    }
+
+    for x, y in values.items():
+        print("Your TDEE for {} lifestyle is: {} calories".format(x, int(bmr * y)))
