a542795b1b9bcde96b0576e8764b292d75e2b61b
Odhran <mahonyodhran@gmail.com>
2022-10-07 00:00:48 +0100

    added if name = main

--- app.py
+++ app.py
@@ -6,23 +6,25 @@ from methods.inputs import get_float, get_gender
 from methods.information import tdee_info
 from models.user import User
 
-AGE = get_float("age")
-HEIGHT = get_float("height")
-WEIGHT = get_float("weight")
-GENDER = get_gender()
 
-new_user = User("Odhran", AGE, WEIGHT, HEIGHT, GENDER)
+if __name__ == "__main__":
+    AGE = get_float("age")
+    HEIGHT = get_float("height")
+    WEIGHT = get_float("weight")
+    GENDER = get_gender()
 
-bmr = mifflin_st_jeor(new_user.age, new_user.height, new_user.weight, new_user.gender)
+    new_user = User("Odhran", AGE, WEIGHT, HEIGHT, GENDER)
 
-print("\n*** TDEE ***")
-# TO BE EXTRACTED#
-tdee_result = input("Would you like to calculate your TDEE? [y/N]: ")
-if tdee_result.lower() == "y":
-    tdee(bmr)
-    info = input("\nWould you like more info on TDEE? [y/N]: ")
-    if info.lower() == "y":
-        tdee_info()
-# TO BE EXTRACTED#
+    bmr = mifflin_st_jeor(new_user.age, new_user.height, new_user.weight, new_user.gender)
 
-print("\nThanks for using the system, goodbye!")
+    print("\n*** TDEE ***")
+    # TO BE EXTRACTED#
+    tdee_result = input("Would you like to calculate your TDEE? [y/N]: ")
+    if tdee_result.lower() == "y":
+        tdee(bmr)
+        info = input("\nWould you like more info on TDEE? [y/N]: ")
+        if info.lower() == "y":
+            tdee_info()
+    # TO BE EXTRACTED#
+
+    print("\nThanks for using the system, goodbye!")
